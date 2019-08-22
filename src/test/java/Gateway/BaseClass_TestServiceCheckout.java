150ac284a0b48dc6ff42d6d0f09ffe6467bcd962
burinskiypavel <54236839+burinskiypavel@users.noreply.github.com>
2019-08-22 18:42:37 +0300

    Update TestSevicesCheckout and base class

--- src/test/java/Gateway/BaseClass_TestServiceCheckout.java
+++ src/test/java/Gateway/BaseClass_TestServiceCheckout.java
@@ -3,6 +3,8 @@ package Gateway;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -10,11 +12,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseClass_TestServiceCheckout  {
     public WebDriver driver;
     public WebDriverWait wait;
+    //WebDriver driver;
+    //WebDriverWait wait;
+    //PageObj pageObj;
+    //MainPage mainPage;
+    //ServicePage servicePage;
+    //PlatformPage platformPage;
+    //ServiceSitePage serviceSitePage;
+
+
+    @BeforeClass
+    void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+        driver.navigate().to("https://www.rbdigitalqa.com/test51/");
+        wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[contains(text(), 'Login')]")));
+        driver.manage().window().maximize();
+
+        //pageObj = new PageObj(driver);
+        //mainPage = new MainPage(driver);
+        //servicePage = new ServicePage(driver);
+        //platformPage = new PlatformPage(driver);
+        //serviceSitePage = new ServiceSitePage(driver);
+    }
 
     @AfterClass
     void afterClass() {
@@ -77,9 +105,7 @@ public class BaseClass_TestServiceCheckout  {
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[contains(text(), 'Create New Account')]")));
     }
 
-
     public void typeInFieldByName(String name, String text) {
-        //webDriver.findElement(By.name(name)).sendKeys(text);
         driver.findElement(By.name(name)).sendKeys(text);
     }
 
@@ -93,4 +119,56 @@ public class BaseClass_TestServiceCheckout  {
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
 
+    public void checkURLcontains(String url){
+        //Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.urlContains(url));
+        Assert.assertTrue(driver.getCurrentUrl().contains(url));
+    }
+
+    public void goIntoServiceByButtonByXpath(String serviceXpath) throws InterruptedException {
+        Thread.sleep(500);
+        Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(serviceXpath)));
+        //int count = 0;
+        //while(driver.findElements(By.xpath(serviceXpath)).size() == 0 && count < 3){
+        //    Thread.sleep(1000);
+        //   count++;
+        //}
+        //int count1 = 0;
+        //while(!driver.findElement(By.xpath(serviceXpath)).isDisplayed() && count1 < 3){
+        //    Thread.sleep(1000);
+        //     count1++;
+        //}
+        clickInFieldByXpath(serviceXpath);
+
+    }
+
+    public void clickInFieldByXpath(String xpath) {
+        driver.findElement(By.xpath(xpath)).click();
+    }
+
+    public void pressGetStartedButton(){
+        clickInFieldByXpath("//a[@href='#'][@class='button']");
+    }
+
+    public void checkText(String element, String text){
+        //driver.findElement(By.xpath("//"+element+"[contains(text(), '"+text+"')]")).getAttribute("innerText");
+        Assert.assertEquals(driver.findElement(By.xpath("//"+element+"[contains(text(), '"+text+"')]")).getAttribute("innerText"), text);
+    }
+
+
+    public void checkElementIsPresent(String css) {
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(css)));
+        Assert.assertTrue(driver.findElement(By.cssSelector(css)).isDisplayed());
+    }
+
+    public void navigate(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void goIntoServiceByPictureByXpath(String serviceXpath) {
+        Wait<WebDriver> wait3 = new WebDriverWait(driver, 20);
+        wait3.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(serviceXpath)));
+        clickInFieldByXpath(serviceXpath);
+    }
 }
--- src/test/java/Gateway/TestServicesCheckouts.java
+++ src/test/java/Gateway/TestServicesCheckouts.java
@@ -5,96 +5,67 @@ import Gateway.pages.PlatformPage;
 import Gateway.pages.ServicePage;
 import Gateway.pages.ServiceSitePage;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 @Test
 public class TestServicesCheckouts extends BaseClass_TestServiceCheckout {
-    //WebDriver driver;
-    //WebDriverWait wait;
-    PageObj pageObj;
-    MainPage mainPage;
-    ServicePage servicePage;
-    PlatformPage platformPage;
-    ServiceSitePage serviceSitePage;
-
-
-    @BeforeClass
-    void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        driver = new ChromeDriver(chromeOptions);
-        driver.navigate().to("https://www.rbdigitalqa.com/test51/");
-        wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[contains(text(), 'Login')]")));
-
-        pageObj = new PageObj(driver);
-        mainPage = new MainPage(driver);
-        servicePage = new ServicePage(driver);
-        platformPage = new PlatformPage(driver);
-        serviceSitePage = new ServiceSitePage(driver);
-    }
 
     @Test(enabled = false)
-    void test_01_Audiobooks_and_eBooksCheckout() {
+    void test_01_audiobooksEBooksService_test51() {
         login("jun5@gmail.com", "12345qw");
-        mainPage.selectServiceByPictureByXpath("//a[@href='#'][@class='service_image rbdigital']");
-        //driver.findElement(By.xpath("//a[@href='#'][@class='service_image rbdigital']")).click();// css=E[A='t']
-        platformPage.CheckRegisterSignInHelpWelcomeTextPresent();
-    }
-
-    @Test
-    void test_02_ArtistWorksService_test51() throws InterruptedException {
-        mainPage.Login("jun5@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/artistworks']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByPictureByXpath("//a[@href='#'][@class='service_image rbdigital']");
+        //platformPage.CheckRegisterSignInHelpWelcomeTextPresent();
         checkURL("https://test-artistworks.artistworks.com/");
-        serviceSitePage.checkText("a", "FREE SAMPLE LESSONS");
     }
 
     @Test
-    void test_03_Newspapers_test51() throws InterruptedException {
+    void test_02_artistWorksService_test51() throws InterruptedException {
         login("jun5@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/newspapers']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/artistworks']");
+        pressGetStartedButton();
+        checkURL("https://test-artistworks.artistworks.com/");
+        checkText("a", "FREE SAMPLE LESSONS");
+    }
+
+    @Test
+    void test_03_newspapers_test51() throws InterruptedException {
+        login("jun5@gmail.com", "12345qw");
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/newspapers']");
+        pressGetStartedButton();
         checkURL("https://www.pressreader.com/catalog");
-        serviceSitePage.checkText("span", "Sign in");
+        checkText("span", "Sign in");
     }
 
     @Test
-    void test_04_Magazines_test51() throws InterruptedException {
+    void test_04_magazines_test51() throws InterruptedException {
         login("jun5@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/magazines']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/magazines']");
+        pressGetStartedButton();
         checkURL("https://www.rbdigitalqa.com/test51/service/magazines/landing?");
-        serviceSitePage.checkText("a", "My Collection");
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("div[class='magazine-card']")));
-        Assert.assertTrue(driver.findElement(By.cssSelector("div[class='magazine-card']")).isDisplayed());
+        checkText("a", "My Collection");
+        checkElementIsPresent("div[class='magazine-card']");
     }
 
     @Test
-    void test_05_Comics_test51() throws InterruptedException {
+    void test_05_comics_test51() throws InterruptedException {
         login("jun5@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/comics']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/test51/service/comics']");
+        pressGetStartedButton();
         checkURL("https://www.rbdigitalqa.com/test51/service/comics/landing?");
-        serviceSitePage.checkText("a", "My Collection");
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("div[class='magazine-card']")));
-        Assert.assertTrue(driver.findElement(By.cssSelector("div[class='magazine-card']")).isDisplayed());
+        checkText("a", "My Collection");
+        checkElementIsPresent("div[class='magazine-card']");
     }
 
     @Test
-    void test_06_TransparentLanguage_rbdigitalinternal() throws InterruptedException {
-        driver.navigate().to("https://www.rbdigitalqa.com/rbdigitalinternal/");
+    void test_06_transparentLanguage_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
         login("jul25@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/transparent_language']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/transparent_language']");
+        pressGetStartedButton();
         checkURL("http://192.168.254.49/rbdigitalinternal/game/rbdgLogin");
         //serviceSitePage.CheckText("a", "My Collection");
         //wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("div[class='magazine-card']")));
@@ -102,65 +73,94 @@ public class TestServicesCheckouts extends BaseClass_TestServiceCheckout {
     }
 
     @Test
-    void test_07_ArtistWorksService_rbdigitalinternal() throws InterruptedException {
-        driver.navigate().to("https://www.rbdigitalqa.com/rbdigitalinternal/");
+    void test_07_artistWorksService_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
         if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
-            mainPage.Logout();
+            logout();
         }
         login("jul25@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/artistworks']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/artistworks']");
+        pressGetStartedButton();
         checkURL("https://test-artistworks.artistworks.com/");
-        serviceSitePage.checkText("a", "FREE SAMPLE LESSONS");
+        checkText("a", "FREE SAMPLE LESSONS");
     }
 
     @Test
-    void test_08_Hoonuit_rbdigitalinternal() throws InterruptedException {
-        driver.navigate().to("https://www.rbdigitalqa.com/rbdigitalinternal/");
+    void test_08_hoonuit_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
         if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
-            mainPage.Logout();
+            logout();
         }
         login("jul25@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/hoonuit']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/hoonuit']");
+        pressGetStartedButton();
         checkURL("https://learnit.hoonuit.com/?from_auth=1");
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("section_heading_Pathways")));
-        serviceSitePage.checkText("h2", "Pathways");
+        checkText("h2", "Pathways");
     }
 
     @Test
-    void test_09_Lawdepot_rbdigitalinternal() throws InterruptedException {
-        driver.navigate().to("https://www.rbdigitalqa.com/rbdigitalinternal/");
+    void test_09_lawdepot_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
         if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
-            mainPage.Logout();
+            logout();
         }
         login("jul25@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/lawdepot']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/lawdepot']");
+        pressGetStartedButton();
         checkURL("https://www.lawdepot.com/libraries/errorRBDG.aspx?errorcode=authentication&id=rbdg&rbid=f1662d2b-aa2a-495a-8de8-65d81c3915b7");
-        serviceSitePage.checkText("h2", "LawDepot Library Subscription");
+        checkText("h2", "LawDepot Library Subscription");
     }
 
     @Test
-    void test_10_Newspapers_rbdigitalinternal() throws InterruptedException {
-        driver.navigate().to("https://www.rbdigitalqa.com/rbdigitalinternal/");
+    void test_10_newspapers_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
         if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
-            mainPage.Logout();
+            logout();
         }
         login("jul25@gmail.com", "12345qw");
-        mainPage.goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/newspapers']");
-        servicePage.pressGetStartedButton();
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/newspapers']");
+        pressGetStartedButton();
         checkURL("https://www.pressreader.com/catalog");
-        serviceSitePage.checkText("span", "Sign in");
+        checkText("span", "Sign in");
     }
 
-    //@AfterMethod
-   // void AfterMethod() {
- //       driver.navigate().to("https://www.rbdigitalqa.com/test51/");
-   //     //driver.switchTo().defaultContent();//exit from iframe
-   //     if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
-    //        //mainPage.Logout();
-      //      logout();
-    //    }
-   // }
+    @Test
+    void test_11_AcornTV_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
+        if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
+            logout();
+        }
+        login("jul25@gmail.com", "12345qw");
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/acorntv']");
+        pressGetStartedButton();
+        checkURL("https://signup-qa.acorn.tv/extendsubscription.html?sessionid=dbdd5785-421e-4531-a9b9-ea6a4c3d9b06");
+        checkText("h4", "Welcome back to Acorn TV");
+    }
+
+    @Test
+    void test_12_pongalo_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
+        if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
+            logout();
+        }
+        login("jul25@gmail.com", "12345qw");
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/pongalo']");
+        pressGetStartedButton();
+        checkURLcontains("https://frontend-dev.pongalo.com/recorded-books/");
+        checkText("p", "PONGALO está disponible en tu teléfono móvil, tableta, computador y TV.");
+    }
+
+    @Test
+    void test_13_methodtestprep_rbdigitalinternal() throws InterruptedException {
+        navigate("https://www.rbdigitalqa.com/rbdigitalinternal/");
+        if (driver.findElements(By.xpath("//div[contains(text(), 'Welcome')]")).size() != 0) {
+            logout();
+        }
+        login("jul25@gmail.com", "12345qw");
+        goIntoServiceByButtonByXpath("//a[@href='//www.rbdigitalqa.com/rbdigitalinternal/service/method-test-prep']");
+        pressGetStartedButton();
+        checkURLcontains("https://dev.methodtestprep.com/student/home/index_v4");
+        checkText("h2", "Student Dashboard");
+    }
 }
