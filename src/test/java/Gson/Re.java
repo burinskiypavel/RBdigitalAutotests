3218b462b93e8c30f211ab0e21481fee6a25b708
burinskiypavel <54236839+burinskiypavel@users.noreply.github.com>
2019-12-27 18:00:33 +0200

    add Re class

---
+++ src/test/java/Gson/Re.java
@@ -0,0 +1,10 @@
+package Gson;
+
+        import java.util.List;
+
+public class Re {
+    //public String locale;
+    //public String time;
+    public String type;
+    //public List<Re> request;
+}
